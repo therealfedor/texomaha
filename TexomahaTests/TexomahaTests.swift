02802a24d3ca8fa01d1576598460759e3ff61b4d
Daniel Goldstein <daniel031403@gmail.com>
2026-09-02 17:58:30 -0400

    Initial Commit

---
+++ TexomahaTests/TexomahaTests.swift
@@ -0,0 +1,19 @@
+//
+//  TexomahaTests.swift
+//  TexomahaTests
+//
+//  Created by Dan Goldstein on 9/2/26.
+//
+
+import Testing
+@testable import Texomaha
+
+struct TexomahaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
